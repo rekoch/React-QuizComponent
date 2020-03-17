b54d6430099c45cd9cb801e01ac056a476783ce8
Res <andreaskocher@mac.com>
2020-03-17 23:42:19 +0100

    add logic if quiz ends to restart

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,7 +16,7 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd
-                    ? <QuizEnd/>
+                    ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
                     : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)}
                                     quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
                 }
@@ -26,6 +26,10 @@ class Quiz extends Component {
 
     showNextQuestion() {
         this.setState({quiz_position: this.state.quiz_position + 1})
+    }
+
+    handleResetClick() {
+        this.setState({quiz_position: 1});
     }
 }
 
