b7a319a791e7b8ebec8176480d4d1779ff30f423
Res <andreaskocher@mac.com>
2020-03-17 23:33:38 +0100

    add logic if wrong answer was hit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {incorrectAnswer: false};
+    }
+
     render() {
         return (
             <main>
@@ -11,16 +16,25 @@ class QuizQuestion extends Component {
                 <section>
                     <ul>
                         {this.props.quiz_question.answer_options.map((answerOption, index) =>
-                                <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answerOption} />
+                            <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index}
+                                                button_text={answerOption}/>
                         )}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer
+                    ? <p className={'error'}>Sorry, that's not right</p>
+                    : null
+                }
             </main>
         )
     }
+
     handleClick(buttonText) {
         if (this.props.quiz_question.answer === buttonText) {
+            this.setState({incorrectAnswer: false});
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState({incorrectAnswer: true});
         }
     }
 }
